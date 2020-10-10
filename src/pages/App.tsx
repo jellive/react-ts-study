f66118e5adeff2bf06a08fdf1ee65e96c676059e
PD Jell <mystral7@naver.com>
2020-10-11 03:04:27 +0900

    add subject.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Subject } from 'rxjs'
 
 function App() {
+  const subject = new Subject()
+  subject.subscribe(
+    (item) => console.log('next', item),
+    (err: Error) => console.error(err),
+    () => console.log('a complete')
+  )
+  subject.next('item')
+  subject.complete()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +30,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
